d58404bf998d51caa5e72db0e953cb988ecaf9b9
Daniel Jones <danielrjones1@gmail.com>
2021-07-16 12:10:19 +0200

    Move Solver to own file

--- CupsPuzzleSolver/Program.cs
+++ CupsPuzzleSolver/Program.cs
@@ -1,46 +1,5 @@
-﻿using System;
-
-namespace CupsPuzzleSolver
+﻿namespace CupsPuzzleSolver
 {
-    public class Solver
-    {
-        private readonly Cups state;
-        private int moveCount = 0;
-
-        public Solver(Cups start)
-        {
-            state = start;
-            state.PrintState();
-            state.CheckValid();
-        }
-
-        public bool Step()
-        {
-            if (state.Solved())
-            {
-                Console.WriteLine("Solved with "+moveCount + " moves.");
-                return false;
-            }
-            
-            var moves = state.GetPossibleMoves();
-            if (moves.Count > 0)
-            {
-                state.PrintMoves();
-                var from = moves[0].Item1;
-                var to = moves[0].Item2;
-                Console.WriteLine("Move " + moveCount + ": Pouring cup " + from + " into cup " + to);
-                state.Move(from, to);
-                moveCount++;
-                state.PrintState();
-                state.CheckValid();
-                return true;
-            }
-
-            Console.WriteLine("No possible moves left, took "+moveCount + " moves.");
-            return false;
-        }
-    }
-
     internal class Program
     {
         private static void Main(string[] args)
---
+++ CupsPuzzleSolver/Solver.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace CupsPuzzleSolver
+{
+    public class Solver
+    {
+        private readonly Cups state;
+        private int moveCount = 0;
+
+        public Solver(Cups start)
+        {
+            state = start;
+            state.PrintState();
+            state.CheckValid();
+        }
+
+        public bool Step()
+        {
+            if (state.Solved())
+            {
+                Console.WriteLine("Solved with "+moveCount + " moves.");
+                return false;
+            }
+            
+            var moves = state.GetPossibleMoves();
+            if (moves.Count > 0)
+            {
+                state.PrintMoves();
+                var from = moves[0].Item1;
+                var to = moves[0].Item2;
+                Console.WriteLine("Move " + moveCount + ": Pouring cup " + from + " into cup " + to);
+                state.Move(from, to);
+                moveCount++;
+                state.PrintState();
+                state.CheckValid();
+                return true;
+            }
+
+            Console.WriteLine("No possible moves left, took "+moveCount + " moves.");
+            return false;
+        }
+    }
+}
