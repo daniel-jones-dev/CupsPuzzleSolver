c49162b2f837f039fc8d5a002398c413abd6c075
Daniel Jones <danielrjones1@gmail.com>
2021-07-16 11:43:31 +0200

    Initial commit.

---
+++ CupsPuzzleSolver/Cup.cs
@@ -0,0 +1,63 @@
+using System;
+
+namespace CupsPuzzleSolver
+{
+    public class Cup
+    {
+        public const int MaxSize = 4;
+
+        private string _contents = "";
+
+        public Cup()
+        {
+        }
+
+        public Cup(string content)
+        {
+            _contents = content;
+        }
+
+        public int Volume => _contents.Length;
+        public bool Empty => Volume == 0;
+        public bool Full => Volume == MaxSize;
+
+        public char Color(int index)
+        {
+            return _contents[index];
+        }
+
+        public char TopColor()
+        {
+            if (Volume == 0) throw new Exception("Cup is empty");
+
+            return Color(Volume - 1);
+        }
+
+        public int NumTopColors()
+        {
+            if (Empty) return 0;
+            for (var i = 1; i < MaxSize; ++i)
+                if (Volume <= i || Color(Volume - i - 1) != TopColor())
+                    return i;
+
+            return MaxSize;
+        }
+
+        public bool CanPourInto(Cup other)
+        {
+            if (other.Full) return false;
+            if (Empty) return false;
+            if (other.Empty) return true;
+            return other.TopColor() == TopColor();
+        }
+
+        public void PourInto(Cup other)
+        {
+            if (!CanPourInto(other)) throw new Exception("Cannot pour this cup into other");
+
+            var colorCount = NumTopColors();
+            other._contents += new String(TopColor(), colorCount);
+            _contents = _contents.Trim(TopColor());
+        }
+    }
+}
---
+++ CupsPuzzleSolver/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CupsPuzzleSolver
+{
+    public class Cups
+    {
+        private readonly Cup[] cups;
+
+        public Cups(string[] cupContents)
+        {
+            cups = new Cup[cupContents.Length];
+            for (var i = 0; i < cupContents.Length; ++i) cups[i] = new Cup(cupContents[i]);
+        }
+
+        public string GetStateString()
+        {
+            var result = "";
+
+            for (var cupIndex = 0; cupIndex < cups.Length; ++cupIndex)
+            {
+                result += " " + cupIndex + " ";
+                result += cupIndex + 1 < cups.Length ? " " : "\n";
+            }
+
+            for (var withinCupIndex = Cup.MaxSize - 1; withinCupIndex >= 0; --withinCupIndex)
+            for (var cupIndex = 0; cupIndex < cups.Length; ++cupIndex)
+            {
+                result += "|";
+                if (cups[cupIndex].Volume > withinCupIndex)
+                    result += cups[cupIndex].Color(withinCupIndex);
+                else
+                    result += " ";
+                result += "|";
+
+                result += cupIndex + 1 < cups.Length ? " " : "\n";
+            }
+
+            for (var cupIndex = 0; cupIndex < cups.Length; ++cupIndex)
+            {
+                result += " ‾ ";
+                result += cupIndex + 1 < cups.Length ? " " : "\n";
+            }
+
+            return result;
+        }
+
+        public List<(int, int)> GetPossibleMoves()
+        {
+            List<(int, int)> result = new List<(int, int)>();
+            for (var i = 0; i < cups.Length; ++i)
+            for (var j = 0; j < cups.Length; ++j)
+                if (i != j && cups[i].CanPourInto(cups[j]))
+                    result.Add((i, j));
+            return result;
+        }
+
+        public void Move(int from, int to)
+        {
+            cups[from].PourInto(cups[to]);
+        }
+        
+        public void PrintState()
+        {
+            Console.WriteLine(GetStateString());
+        }
+
+        public void PrintMoves()
+        {
+            var moves = GetPossibleMoves();
+            foreach ((var from, var to) in moves)
+            {
+                Console.WriteLine("Can pour cup " + from + " into cup " + to);
+            }
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string[] cupContents = {"", "POPO", "OPOP"};
+            
+            
+            
+            var cups = new Cups(cupContents);
+            cups.PrintState();
+            cups.PrintMoves();
+
+            Console.WriteLine("Pouring cup 1 into cup 0");
+            cups.Move(1,0);
+            cups.PrintState();
+            cups.PrintMoves();
+            
+            
+        }
+    }
+}
