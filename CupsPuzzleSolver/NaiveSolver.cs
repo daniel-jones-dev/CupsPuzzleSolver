b2c0fd0f2961842b325f7d8424c4cc7489cc6951
Daniel Jones <danielrjones1@gmail.com>
2021-07-16 15:55:14 +0200

    NaiveSolver now tracks explored states and can produce best found solution

--- CupsPuzzleSolver/NaiveSolver.cs
+++ CupsPuzzleSolver/NaiveSolver.cs
@@ -1,43 +1,132 @@
 using System;
+using System.Collections.Generic;
 
 namespace CupsPuzzleSolver
 {
     public class NaiveSolver
     {
-        private readonly Cups state;
-        private int moveCount;
+        // Store collection, foreach state: moves to reach, and the previous state
+        private readonly Dictionary<Cups, (int moves, Cups? prev)> _explored;
+
+        // List of states yet to be explored, the steps to get there, and the state we reached them from
+        private readonly List<(Cups state, int moves, Cups? prev)> _toExplore;
+
+        private (int, Cups?) _bestSolution;
 
         public NaiveSolver(Cups start)
         {
-            state = start;
-            state.PrintState();
-            state.CheckValid();
+            _toExplore = new List<(Cups, int, Cups?)> {(start, 0, null)};
+            _explored = new Dictionary<Cups, (int, Cups?)>();
+        }
+
+        private bool Explore(Cups to, Cups? from)
+        {
+            if (from == null)
+            {
+                _explored.Add(to, (0, null));
+                return true;
+            }
+
+            var (fromCost, _) = _explored[from];
+
+            if (_explored.ContainsKey(to))
+            {
+                var (currCost, _) = _explored[to];
+                if (fromCost + 1 < currCost)
+                {
+                    _explored[to] = (fromCost + 1, from);
+                    return true;
+                }
+            }
+            else
+            {
+                _explored.Add(to, (fromCost + 1, from));
+                return true;
+            }
+
+            return false;
         }
 
         public bool Step()
         {
-            if (state.Solved())
+            // Take the next state to be explored
+            if (_toExplore.Count == 0)
             {
-                Console.WriteLine("Solved with " + moveCount + " moves.");
+                Console.WriteLine("Fully explored tree");
                 return false;
             }
 
-            var moves = state.GetPossibleMoves();
-            if (moves.Count > 0)
+            var (currState, currMoves, prevState) = _toExplore[0];
+            _toExplore.RemoveAt(0);
+
+            // Console.WriteLine("==============================================");
+            // Console.WriteLine("Checking state:");
+            // currState.PrintState();
+
+            if (_explored.TryGetValue(currState, out (int, Cups?) foundEntry))
             {
-                state.PrintMoves();
-                var from = moves[0].Item1;
-                var to = moves[0].Item2;
-                Console.WriteLine("Move " + moveCount + ": Pouring cup " + from + " into cup " + to);
-                state.Move(from, to);
-                moveCount++;
-                state.PrintState();
-                state.CheckValid();
-                return true;
+                var (foundMoves, _) = foundEntry;
+                if (currMoves >= foundMoves)
+                    // Console.WriteLine("State already encountered");
+                    return true;
+
+                _explored[currState] = (currMoves, prevState);
+            }
+            else
+            {
+                _explored.Add(currState, (currMoves, prevState));
+                Console.WriteLine(_explored.Count + " states have been explored");
             }
 
-            Console.WriteLine("No possible moves left, took " + moveCount + " moves.");
-            return false;
+            if (currState.Solved())
+            {
+                var (bestMoveCount, bestSolution) = _bestSolution;
+                if (bestSolution == null || currMoves < bestMoveCount)
+                {
+                    Console.WriteLine("Found a solution with " + currMoves + " moves.");
+                    _bestSolution = (currMoves, currState);
+                }
+            }
+            else
+            {
+                // Enumerate possible moves from this state, and add them all to the to-explore list
+                var moveList = currState.GetPossibleMoves();
+                // Console.WriteLine(moveList.Count + " possible moves");
+                foreach (var (from, to) in moveList)
+                {
+                    // Console.WriteLine("Pouring cup " + from + " into cup " + to + ", resulting in:");
+                    var nextState = (Cups) currState.Clone();
+                    nextState.Move(from, to);
+                    // nextState.PrintState();
+                    _toExplore.Add((nextState, currMoves + 1, currState));
+                }
+            }
+
+            return true;
+        }
+
+        public void PrintBestSolution()
+        {
+            var (_, solvedState) = _bestSolution;
+            if (solvedState == null)
+            {
+                Console.WriteLine("No solution found.");
+                return;
+            }
+            var currState = solvedState;
+            List<Cups> solution = new();
+
+            while (true)
+            {
+                solution.Insert(0, currState);
+                var (moves, prevState) = _explored[currState];
+
+                if (prevState == null) break;
+                currState = prevState;
+            }
+
+            Console.WriteLine(solution.Count - 1 + " moves in solution");
+            foreach (var state in solution) state.PrintState();
         }
     }
 }
--- CupsPuzzleSolver/Program.cs
+++ CupsPuzzleSolver/Program.cs
@@ -4,15 +4,22 @@
     {
         private static void Main(string[] args)
         {
-            var puzzle1 = "-,POPO,OPOP";
-            var puzzle2 = "YYYB,DDBB,DDBY,-,-";
-
-            var cups = new Cups(puzzle2);
+            var puzzle1 = "RRR,R";
+            var puzzle2 = "-,POPO,OPOP";
+            var puzzle3 = "YYYB,DDBB,DDBY,-,-"; // Note: second empty cup is not needed
+            var puzzle4 = "YYBO,BBYO,OYBO,-,-"; // Note: second empty cup is not needed
+            var puzzle5 = "BYYB,DOOD,DYOB,OYDB,-,-"; // Note: second empty cup is not needed
+            var puzzle6 = "YYBP,YpBP,pPpB,PpBY,-,-";
+            var puzzle7 = "GuPP,PODu,DGPG,ODuG,OuOD,-,-"; // Currently unsolvable
+            
+            var cups = new Cups(puzzle6);
 
             var solver = new NaiveSolver(cups);
             while (solver.Step())
             {
             }
+
+            solver.PrintBestSolution();
         }
     }
 }
